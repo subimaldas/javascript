d190f3d31f3585bafbd9e2e38a9ec40819677a0e
Subimal Das <er.subimaldas@gmail.com>
2023-08-04 22:59:09 +0530

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,8 @@
+var a = 67 // a contains 67
+console.log(a)
+a = "subimal"
+console.log(a)
+//let 8subimal = 7 // Not allowed this will throw an error
+// let var = 7
+
+//We can change variables in (runtime) in Js
