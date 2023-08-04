a58367114e2cda43976adfb27f1d19943fe8622e
Subimal Das <er.subimaldas@gmail.com>
2023-08-05 01:36:11 +0530

    Create 4.1_Strings.js

---
+++ 4.1_Strings.js
@@ -0,0 +1,21 @@
+let name = "Subimal"
+// console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+let friend = 'Prakash'
+console.log(friend)
+// let myfriend = "Shubh' // Dont do this
+// console.log(myfriend)
+
+// Template literals
+let man1 = "Pramod"
+let man2 = "Nikhil"
+// Nikhil is a friend of Pramod
+let sentence = `${man2} is a friend of ${man1}`
+console.log(sentence)
+
+// Escape Sequence Characters
+let fruit = 'Bana\'na'
+ console.log(fruit)
+// let fruit = 'Bana\"na"
+// console.log(fruit)
