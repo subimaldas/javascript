93ee1e9ec21dd79ecd0db4e6325c19d774301034
Subimal Das <er.subimaldas@gmail.com>
2023-08-04 23:01:18 +0530

    Create const_var_let.js

---
+++ const_var_let.js
@@ -0,0 +1,19 @@
+console.log("JavaScript tutorial 3: var, let and const")
+// var a = 45;
+// var a = "p"
+let b = "Subimal";
+const author = "Subimal"
+//author = 5 // Throws an error because constant cannot be changed
+b = 4
+const sagar = 0;
+let c = null
+let d  = undefined  
+{
+  let b = 'this'
+  console.log(b)
+}
+console.log(b)
+
+//let is block scoped | can't be redeclared
+//var is gone now | var introduces bug 
+//const for determining fix value ,can't be changed
