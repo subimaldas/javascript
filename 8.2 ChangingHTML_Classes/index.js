089e864f4f4ce8d40846a93f05ad997d9e5cb759
Subimal Das <er.subimaldas@gmail.com>
2023-08-09 21:38:16 +0530

    Create index.js

---
+++ 8.2 ChangingHTML_Classes/index.js
@@ -0,0 +1,5 @@
+first.className = "text-black red"
+first.classList.add("red")
+first.classList.remove("red")
+first.classList.contains("red")  //check  the class or not return(true & false)
+first.classList.toggle("red")    //if exist skip if not add class
